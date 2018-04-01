e28b3505a7dc9d402921d7faf44d596e5e014f30
GnomoP <ks1202@protonmail.ch>
2018-04-01 13:56:36 -0300

    root@kali-kezio 2018-04-01 13:56:36

--- .tailcolor.pl
+++ .tailcolor.pl
@@ -6,58 +6,44 @@ LINE:
     # For a table of colors, see color-codes
 
     # Bash verbose output
-    s{
-      (^\++.*)
-    }{
-      \e[1;37;41m$1  # Bold white on Red BG
-      \e[m
-    }agx and next;
+    s{(^\++.*)}{\e[1;37;41m$1\e[m}ag and next;
 
     # TODO get actual 'whoami' and 'hostname' outputs
-    if
-    (
-    m{
-      ^\s*
-      ( \[master\ [a-z0-9]{7,}\] )|
-      ( root\@kali-kezio\ \d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2} )
-      \s*$
-    }ax
-    ) {
-      s{
+    if ( m{ ^\s*
         ( \[master\ [a-z0-9]{7,}\] )
-        \ (root\@kali-kezio\ \d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2} )
-      }{\e[1;34m$1\e[1;31m$2\e[m}agx;
+      | ( root\@kali-kezio\ \d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2} )
+            \s*$}ax )
+    {
+      s{ ( \[master\ [a-z0-9]{7,}\] )
+       \ (root\@kali-kezio\ \d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2} ) }
+       {\e[1;34m$1 \e[1;31m$2\e[m}agx;
 
       next;
     }
 
-    if (
-    m{
-      ( \d+\ files?\ changed )|
-      ( \d+\ deletions?\(-\) )|
-      ( \d+\ insertions?\(\+\) )
-    }ax
-    ) {
-      s{
-        ( \d+ )\ ( files?\ changed,? )
-      }{\e[1;36m$1 \e[0;36m$2 \e[m}agx;
+    if ( m{ ^\s* ( \d+\ files?\ changed )|
+                 ( \d+\ deletions?\(-\) )|
+                 ( \d+\ insertions?\(\+\) ) \s*$ }ax )
+    {
+      s{ ( \d+ )\ ( files?\ changed,? ) }
+       {\e[1;36m$1 \e[0;36m$2 \e[m}agx;
 
-      s{
-        ( \d+ )\ ( deletions? \( ) (-) ( \) ,? )
-      }{\e[1;31m$1 \e[0;31m$2\e[1;31m$3\e[0;31m$4\e[m}agx;
+      s{ ( \d+ )\ ( deletions? \( ) (-) ( \) ,? ) }
+       {\e[1;31m$1 \e[0;31m$2\e[1;31m$3\e[0;31m$4\e[m}agx;
 
-      s{
-        ( \d+ )\ ( insertions? \( ) (\+) ( \),? )
-      }{\e[1;32m$1 \e[0;32m$2\e[1;32m$3\e[0;32m$4\e[m}agx;
+      s{( \d+ )\ ( insertions? \( ) (\+) ( \),? ) }
+       {\e[1;32m$1 \e[0;32m$2\e[1;32m$3\e[0;32m$4\e[m}agx;
 
       next;
     }
 
-    if (m/^\s+[A-Za-z0-9]+\.\.[A-Za-z0-9]+\s+master -> master/) {
-      s/([A-Za-z0-9]+\.\.[A-Za-z0-9]+)/\e[1;33m$1\e[m/g;
-                            s/(master)/\e[0;34m$1\e[m/g;
-                                s/(->)/\e[1;32m$1\e[m/g;
-      next;
+    if ( m{ ^\s* [a-z0-9]{7,} \.\. [a-z0-9]{7,} \s* \w+\ -> \w+ \s*$ }gx )
+    {
+      s{ ( [a-z0-9]{7,} ) \.\. ( [a-z0-9]{7,} ) }
+       {\e[1;33m$1 ... $2\e[m}agx;
+
+      s{ ( .* )\ ( -> )\ ( .* ) }
+       {\e[0;34m$1 \e[1;32m$2 \e[0;34m]$3\e[m}agx;
     }
 
    #
