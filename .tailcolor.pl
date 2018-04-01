bc50511e251bbb3e75d243cd7cf12816ab07c415
GnomoP <ks1202@protonmail.ch>
2018-04-01 12:18:06 -0300

    root@kali-kezio 2018-04-01 12:18:06

--- .tailcolor.pl
+++ .tailcolor.pl
@@ -15,13 +15,13 @@ LINE:
 
     s{
       (^\s*
-        \[master\s*[A-Za-z0-9]{7,}\]
+        \[master [A-Za-z0-9]{7,}\]
       \s*)
       (\s*
         $commitname
       \s*$)
     }{
-      \e[1;34m\1  # Bold Blue on BG
+      \e[1;34m$1  # Bold Blue on BG
       \e[2;31m$2  # Bold Red on BG
       \e[m
     }ax and next;
