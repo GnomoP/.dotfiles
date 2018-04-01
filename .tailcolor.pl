3e0ffaaf60ecd8646a8550a7be36dba83f17af1b
GnomoP <ks1202@protonmail.ch>
2018-03-31 23:10:30 -0300

    root@kali-kezio 2018-03-31 23:10:30

--- .tailcolor.pl
+++ .tailcolor.pl
@@ -6,30 +6,30 @@ LINE:
 
     if (m/^\[master [A-Za-z0-9]+\] $commitname\s+$/) {
       s/(\[master [A-Za-z0-9]+\])/\e[1;34m\1\e[m/g;
-                  s/$commitname/\e[0;31m$&\e[m/g;
+                  s/($commitname)/\e[0;31m\1\e[m/g;
       next;
     }
 
     if (m/^\s+[A-Za-z0-9]+\.\.[A-Za-z0-9]+\s+master -> master/) {
-      s/[A-Za-z0-9]+\.\.[A-Za-z0-9]+/\e[1;33m$&\e[m/g;
-                            s/master/\e[0;34m$&\e[m/g;
-                                s/->/\e[1;32m$&\e[m/g;
+      s/([A-Za-z0-9]+\.\.[A-Za-z0-9]+)/\e[1;33m\1\e[m/g;
+                            s/(master)/\e[0;34m\1\e[m/g;
+                                s/(->)/\e[1;32m\1\e[m/g;
       next;
     }
 
     if (m/(\d+ file(s)? changed(, )?)|(\d+ insertion(s)?\(\+\)(, )?)|(\d+ deletion(s)?\(-\)(, )?)/) {
-      s/\d+ insertion(s)?\(\+\)(, )?/\e[32m$&\e[m/g;
-        s/\d+ deletion(s)?\(-\)(, )?/\e[31m$&\e[m/g;
-         s/\d+ file(s)? changed(, )?/\e[36m$&\e[m/g;
+      s/(\d+ insertion(s)?\(\+\)(, )?)/\e[32m\1\e[m/g;
+        s/(\d+ deletion(s)?\(-\)(, )?)/\e[31m\1\e[m/g;
+         s/(\d+ file(s)? changed(, )?)/\e[36m\1\e[m/g;
     }
 
     if (m/('?master'?)|('?origin'?)/) {
-      s/'?master'?/\e[1;34m$&\e[m/g;
-      s/'?origin'?/\e[1;34m$&\e[m/g;
+      s/('?master'?)/\e[1;34m\1\e[m/g;
+      s/('?origin'?)/\e[1;34m\1\e[m/g;
     }
 
     if (m/\W*$commitname\W*/) {
-      s/\W*$commitname\W*/\e[1;31m$&\e[m/g;
+      s/(\W*$commitname\W*)/\e[1;31m\1\e[m/g;
     }
   } continue {
     print or die "-p destination: $!\n";
