cc9f144e2326b618648990b075af15d63f6d14cc
GnomoP <ks1202@protonmail.ch>
2018-03-31 21:09:40 -0300

    root@kali-kezio 2018-03-31 21:09:40

--- .tailcolor.pl
+++ .tailcolor.pl
@@ -1,14 +1,12 @@
 LINE:
   while (<>) {
-    continue;
+    #   my $commitname = `whoami | tr -d '\n'`.'@'.`hostname | tr -d '\n'`.' \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}';
 
-    my $commitname = `whoami | tr -d '\n'`.'@'.`hostname | tr -d '\n'`.' \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}';
-
-    if (m/\[master \d+\] $commitname/) {
-        s/\[master \d+\] $commitname/\e[1;34m$&\e[m/;
-    }
-    elsif (m/$commitname/) {
-           s/$commitname/\e[1;31m$&\e[m/;
+    #   if (m/\[master \d+\] $commitname/) {
+    #       s/\[master \d+\] $commitname/\e[1;34m$&\e[m/;
+    #   }
+    #   elsif (m/$commitname/) {
+    #          s/$commitname/\e[1;31m$&\e[m/;
     }
   } continue {
     print or die "-p destination: $!\n";
