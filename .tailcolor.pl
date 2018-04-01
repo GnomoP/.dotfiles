ea2a616d4493d2b2c95380e281087fd680fcf232
GnomoP <ks1202@protonmail.ch>
2018-03-31 21:08:03 -0300

    root@kali-kezio 2018-03-31 21:08:03

--- .tailcolor.pl
+++ .tailcolor.pl
@@ -2,10 +2,10 @@ LINE:
   while (<>) {
     my $commitname = `whoami | tr -d '\n'`.'@'.`hostname | tr -d '\n'`.' \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}';
     if (m/\[master \d+\] $commitname/) {
-        s/\[master \d+\] $commitname/\e[1;34m$&\e[m/g;
+        s/\[master \d+\] $commitname/\e[1;34m$&\e[m/;
     }
     elsif (m/$commitname/) {
-           s/$commitname/\e[1;31m$&\e[m/g;
+           s/$commitname/\e[1;31m$&\e[m/;
     }
   } continue {
     print or die "-p destination: $!\n";
