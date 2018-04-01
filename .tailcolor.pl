604e0371ab28439c0cddd95ed1ff044c8bf19012
GnomoP <ks1202@protonmail.ch>
2018-03-31 21:20:03 -0300

    root@kali-kezio 2018-03-31 21:20:03

--- .tailcolor.pl
+++ .tailcolor.pl
@@ -2,8 +2,8 @@ LINE:
   while (<>) {
     my $commitname = `whoami | tr -d '\n'`.'@'.`hostname | tr -d '\n'`.' \d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}';
 
-    if (m/\[master \d+\] $commitname/) {
-        s/\[master \d+\] $commitname/\e[1;34m$&\e[m/;
+    if (m/\[master [A-Za-z0-9]+\] $commitname/) {
+        s/\[master [A-Za-z0-9]+\] $commitname/\e[1;34m$&\e[m/;
     }
     elsif (m/$commitname/) {
            s/$commitname/\e[1;31m$&\e[m/;
